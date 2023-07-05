e27171002a98092d0cb0463d89910cad1e673333
Izaias Lima <izaiasmlima@gmail.com>
2023-07-05 17:05:47 -0300

    Configuracao do Material-UI

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,19 @@
+import { Box, Button, Container, Link, Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <div>
-      <h1>OI</h1>
-    </div>
+    <Container maxWidth="sm">
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Clone do Youtube
+      </Typography>
+
+      <Box>
+        <Button color="primary" variant="contained" >teste</Button>
+        <Button color="secondary" variant="outlined" >teste</Button>
+      </Box>
+
+    </Box>
+  </Container>
   );
 }
